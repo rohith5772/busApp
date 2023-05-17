610a5440ffc0418a9fcd81dd21ac924d881012b4
rohith5772 <rohith.c15@iiits.in>
2023-05-17 22:55:29 +0530

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/font-awesome/css/font-awesome.min.css'; 
 import ReactGA from 'react-ga';
-ReactGA.initialize('UA-262916911-1');
+ReactGA.initialize('276609735');
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
